a069b317c0c6046ae052312eb2f392f82b47d7df
Mustafa Hasanain <m7asanain@gmail.com>
2025-04-20 19:31:37 +0300

    develop the navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import { navLinks } from "../constants";
+
+const NavBar = () => {
+  // tracking if the user has scrolled
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 10;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // cleanup the event listener when the component is unmounted
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
+      <div className="inner">
+        <a href="#hero" className="logo">
+          Mustafa Hasanain
+        </a>
+
+        {/* this only appear on desktop version */}
+        <nav className="desktop">
+          <ul>
+            {navLinks.map(({ link, name }) => (
+              <li key={name} className="group">
+                <a href={link}>
+                  <span>{name}</span>
+                  <span className="underline" />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <a href="#contact" className="contact-btn group">
+          <div className="inner">
+            <span>Contact me</span>
+          </div>
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
