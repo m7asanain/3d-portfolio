0608cfa3021faaa3210d05db9366c2e101d5577d
Mustafa Hasanain <m7asanain@gmail.com>
2025-04-22 16:32:51 +0300

    finish developing the experience section

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,8 +1,71 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import { expCards } from "../constants";
 import TitleHeader from "../components/TitleHeader";
 import GlowCard from "../components/GlowCard";
 
+gsap.registerPlugin(ScrollTrigger);
+
 const Experience = () => {
+  // left cards animation
+  // animate left and fade in
+  useGSAP(() => {
+    gsap.utils.toArray(".timeline-card").forEach((card) => {
+      gsap.from(card, {
+        xPercent: -100,
+        opacity: 0,
+        transformOrigin: "left left",
+        duration: 1,
+        ease: "power2.inOut",
+        // trigger the animation when the card is 80% of the way down the screen
+        scrollTrigger: {
+          trigger: card,
+          start: "top 80%",
+        },
+      });
+    });
+
+    // timeline animation
+    gsap.to(".timeline", {
+      transformOrigin: "bottom bottom",
+      // animate the timeline height over 1 second
+      ease: "power1.inOut",
+      // trigger the animation when the timeline is at the top of the screen
+      // and end it when the timeline is at 70% down the screen
+      scrollTrigger: {
+        trigger: ".timeline",
+        start: "top center",
+        end: "70% center",
+        // update the animation as the user scrolls
+        onUpdate: (self) => {
+          // Scale the timeline height as the user scrolls
+          // from 1 to 0 as the user scrolls up the screen
+          gsap.to(".timeline", {
+            scaleY: 1 - self.progress,
+          });
+        },
+      },
+    });
+
+    // text animation (fading)
+    gsap.utils.toArray(".expText").forEach((text) => {
+      // animate fade and moving
+      gsap.from(text, {
+        opacity: 0,
+        xPercent: 0,
+        duration: 1,
+        ease: "power2.inOut",
+        // trigger the animation when the text is 60% down the screen
+        scrollTrigger: {
+          trigger: text,
+          start: "top 60%",
+        },
+      });
+    });
+  }, []);
+
   return (
     <section
       id="experience"
@@ -24,6 +87,38 @@ const Experience = () => {
                       <img src={card.imgPath} alt="exp-img" />
                     </div>
                   </GlowCard>
+                </div>
+
+                <div className="xl:w-4/6">
+                  <div className="flex items-start">
+                    <div className="timeline-wrapper">
+                      <div className="timeline" />
+                      <div className="gradient-line w-1 h-full" />
+                    </div>
+                    <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
+                      <div className="timeline-logo">
+                        <img src={card.logoPath} alt="logo" />
+                      </div>
+                      <div>
+                        <h1 className="font-semibold text-3xl">{card.title}</h1>
+                        <p className="my-5 text-white-50">
+                          🗓️&nbsp;{card.date}
+                        </p>
+                        <p className="text-[#839CB5] italic">
+                          Responsibilities
+                        </p>
+                        <ul className="list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50">
+                          {card.responsibilities.map(
+                            (responsibility, index) => (
+                              <li key={index} className="text-lg">
+                                {responsibility}
+                              </li>
+                            )
+                          )}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             ))}
